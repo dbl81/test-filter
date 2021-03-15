9afb12ffa2386b77fe2388bde9eb764f6d1989ce
gianfelice.catini@gmail.com <gianfelice.catini@gmail.com>
2021-03-15 20:15:03 +0100

    first commit

---
+++ src/main/java/org/quarkus/restclient/RestClient.java
@@ -0,0 +1,19 @@
+package org.quarkus.restclient;
+
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/")
+@ApplicationScoped
+@RegisterRestClient(configKey = "test-filter")
+public interface RestClient {
+
+    @GET
+    @Produces(MediaType.TEXT_HTML)
+    public String hello();
+}
---
+++ src/test/java/org/quarkus/restclient/RestClientTest.java
@@ -0,0 +1,20 @@
+package org.quarkus.restclient;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
+@QuarkusTest
+public class RestClientTest {
+
+    @Test
+    public void testHelloEndpoint() {
+        given()
+          .when().get("/")
+          .then()
+             .statusCode(200);
+    }
+
+}
