426be3b0c36893882c1c09066430ff60f1fbeb9a
gianfelice.catini@gmail.com <gianfelice.catini@gmail.com>
2021-03-15 22:48:26 +0100

    clientrequest and response implemented

--- src/main/java/org/quarkus/restclient/LoggingFilter.java
+++ src/main/java/org/quarkus/restclient/LoggingFilter.java
@@ -2,15 +2,22 @@ package org.quarkus.restclient;
 
 import org.jboss.logging.Logger;
 
-import javax.ws.rs.container.ContainerRequestContext;
-import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.client.ClientRequestContext;
+import javax.ws.rs.client.ClientRequestFilter;
+import javax.ws.rs.client.ClientResponseContext;
+import javax.ws.rs.client.ClientResponseFilter;
 import java.io.IOException;
 
-public class LoggingFilter implements ContainerRequestFilter    {
+public class LoggingFilter implements ClientRequestFilter, ClientResponseFilter {
     private static final Logger LOG = Logger.getLogger(LoggingFilter.class);
 
     @Override
-    public void filter(ContainerRequestContext containerRequestContext) throws IOException {
-        LOG.info("executing filter");
+    public void filter(ClientRequestContext clientRequestContext) throws IOException {
+        LOG.info("request");
+    }
+
+    @Override
+    public void filter(ClientRequestContext clientRequestContext, ClientResponseContext clientResponseContext) throws IOException {
+        LOG.info("response");
     }
 }
